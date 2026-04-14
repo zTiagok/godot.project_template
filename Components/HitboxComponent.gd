a7e6fe81ccc958df2baef4bf459b25069c6010d4
Tiago Braga <ztiagok@gmail.com>
2026-04-14 12:00:55 -0300

    refactor: HurtboxComponents to enheritage HitboxComponent functions.

--- Components/HitboxComponent.gd
+++ Components/HitboxComponent.gd
@@ -2,21 +2,7 @@ class_name HitboxComponent extends Area2D
 
 @onready var collision : CollisionShape2D = $CollisionShape2D
 
-var currentTool : DataTypes.ToolTypes
-var damage : float = 1
-
-
 func _on_area_entered(area: Area2D) -> void:
-  # Caso exista um hurtbox, pegará o pai.
+  # Caso exista um hurtbox, enviará os dados contidos na hitbox.
   if area is HurtboxComponent:
-    var target = area.get_parent()
-
-    # Caso o pai seja um StaticBody, verificará se a ferramenta coincide.
-    if target is StaticEntity:
-      currentTool = get_parent().currentTool
-
-      print("Needed Tool: ", target.neededTool)
-      print("Current Tool: ", currentTool)
-      if target.neededTool == currentTool:
-        print("Tá batendo a ferramenta")
-        area.onHit(damage)
+    area.onHitboxContact(self)
--- Components/HurtboxComponent.gd
+++ Components/HurtboxComponent.gd
@@ -2,6 +2,19 @@ class_name HurtboxComponent extends Area2D
 
 @onready var collision : CollisionShape2D = $CollisionShape2D
 
-func onHit(damage: float) -> void:
-  print("DEU DANO!: ", damage)
-  pass
+func onHitboxContact(hitbox: HitboxComponent) -> void:
+  var attacker = hitbox.owner
+  var damage = hitbox.damage
+  var target = get_parent()
+
+  if target is StaticEntity:
+    # Caso seja um StaticEntity e não tenha as mesmas ferramentas, irá ignorar
+    # o código.
+    if attacker.currentTool != target.neededTool:
+      return
+
+  applyDamage(damage)
+
+
+func applyDamage(damage: float) -> void:
+  print("APLICANDO DANO: ", damage)
